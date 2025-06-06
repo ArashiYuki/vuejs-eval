c31e1dcdf8b47dd7e93ba22ef5e912c4c00bd6f6
Lea Pateau <lea.pateau0@gmail.com>
2025-06-06 16:59:24 +0200

    ✨ liste et suppression client  faute de frappe entreprise

---
+++ frontend/src/components/ClientItem.vue
@@ -0,0 +1,62 @@
+<!-- ClientItem.vue -->
+<template>
+  <li class="d-flex justify-content-between align-items-center text-secondary py-2">
+    <div class="d-flex gap-3" style="flex: 1; min-width: 0;">
+		<span style="min-width: 110px; white-space: nowrap;">
+			{{ client.nom }} {{ client.prenom }}
+		</span>
+		<span style="min-width: 110px; white-space: nowrap;">
+			{{ client.entreprise }}
+		</span>
+		<span style="min-width: 110px; white-space: nowrap;">
+        	{{ client.dateAjout }}
+      	</span>
+    </div>
+
+    <div class="d-flex gap-3 align-items-center" style="min-width: 360px; justify-content: flex-end;">
+
+      <button @click="onDelete" class="btn btn-sm btn-danger">
+        Supprimer
+      </button>
+      <button @click="onEdit" class="btn btn-sm btn-primary">
+        Éditer
+      </button>
+    </div>
+  </li>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { useClientStore } from '@/stores/client'
+import { storeToRefs } from 'pinia'
+
+// Récupérer les props
+const props = defineProps({
+  client: {
+    type: Object,
+    required: true
+  }
+})
+
+const emit = defineEmits(['delete'])
+
+const router = useRouter()
+
+const clientStore = useClientStore()
+const { getClientById } = clientStore;
+
+const onDelete = () => {
+  emit('delete', props.client.id)
+}
+
+const onEdit = () => {
+  router.push(`/clients/edit/${props.client.id}`)
+}
+</script>
+
+
+<style scoped>
+li {
+  font-weight: normal;
+}
+</style>
--- frontend/src/views/ClientsView.vue
+++ frontend/src/views/ClientsView.vue
@@ -1,5 +1,59 @@
 <template>
-	<div class="container py-5">
-		<h1><i class="bi bi-chevron-down me-2"></i>Liste des clients</h1>
+  <section class="container py-5">
+	<div class="d-flex justify-content-between">
+		<h1 class="mb-4"><i class="bi bi-chevron-down me-2"></i>Liste des clients</h1>
+		<BButton
+          iconLeft="plus-circle"
+          variant="outline-primary"
+          class="w-100 mt-3"
+          @click="$router.push('/clients/create')"
+        >
+          Ajouter une client
+        </BButton>
 	</div>
+
+    <ul class="list-unstyled">
+      <ClientItem
+        v-for="client in clients"
+        :key="client.id"
+        :client="client"
+        @delete="handleDeleteClient"
+      />
+    </ul>
+
+    <p v-if="!clients.length" class="text-center text-muted">
+      Aucune client disponible.
+    </p>
+  </section>
 </template>
+
+<script setup>
+import { onBeforeMount } from 'vue'
+import { useClientStore } from '@/stores/client'
+import { storeToRefs } from 'pinia'
+import ClientItem from '@/components/ClientItem.vue'
+import BButton from '@/components/BButton.vue'
+
+const clientStore = useClientStore()
+const { clients } = storeToRefs(clientStore)
+const { getClients, deleteClient } = clientStore
+
+onBeforeMount(async () => {
+  await getClients()
+})
+
+const handleDeleteClient = async (id) => {
+  if (!confirm('Voulez-vous vraiment supprimer cette client ?')) return
+
+  try {
+    await deleteClient(id)
+    const index = clients.value.findIndex(f => f.id === id)
+    if (index !== -1) {
+      clients.value.splice(index, 1)
+    }
+  } catch (error) {
+    alert('Erreur lors de la suppression.')
+    console.error(error)
+  }
+}
+</script>
